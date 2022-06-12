17101e18fba2d2b7b2985565acc1db0b2951dfed
Vanessa Montiel <32447570+vmontielmg@users.noreply.github.com>
2022-06-12 13:44:07 -0700

    package cards info and style

--- beta-test-app/src/components/Packages.js
+++ beta-test-app/src/components/Packages.js
@@ -6,49 +6,63 @@ function Packages() {
         },
         packageCard: {
             border: '1px solid #000',
-            borderRadius: 50,
+            borderRadius: 30,
+            boxShadow: "-1px 5px 5px #0D3B66",
             display: "inline-block",
-            width: "25%",
-            height: "20%"
+            width: "27%",
+            height: 360,
+            margin: "3px 5px",
+            padding: "2px 5px",
+            backgroundColor: "#FFFEFB"
+        },
+        submit: {
+            border: "0.5px solid #0D3B66",
+            borderRadius: 8,
+            backgroundColor: "#2084E2",
+            color: "#FFF",
+            width: "40%",
+            height: 40,
+            marginTop: 20,
+            fontSize: "large"
+        },
+        text: {
+            color: "#0D3B66"
         }
     }
     return ( 
         <div className="packages" style={packageStyles.packages}>
-            <h1>Select the right Package for your business!</h1>
+            <h1 style={packageStyles.text}>Select the right Package for your business!</h1>
 
             <div className="package" id="freeTier" style={packageStyles.packageCard}>
                 <h3>Free Tier</h3>
                 <p>This is our free subscription tier that includes a few basic features.</p>
                 <ul>
-                    <li>1 product</li>
-                    <li>access to post product...?</li>
-                    <li>...?</li>
+                    <li>Post 1 product to our beta testers</li>
+                    <li>Available access to all testers</li>
                 </ul>
-                <button type="button">Select</button>
+                <button type="button" style={packageStyles.submit}>Select</button>
             </div>
 
             <div className="package" id="basicTier" style={packageStyles.packageCard}>
                 <h3>Basic Tier</h3>
                 <p>This is our basic subscription tier that includes ...</p>
                 <ul>
-                    <li>Up to 5 products</li>
-                    <li>access to post product...?</li>
-                    <li>access to premium beta testers</li>
+                    <li>Post up to 5 product to our beta testers</li>
+                    <li>Access to premium beta testers</li>
+                    <li>+ Free Tier features</li>
                 </ul>
-                <button type="button">Select</button>
+                <button type="button" style={packageStyles.submit}>Select</button>
             </div>
 
             <div className="package" id="premiumTier" style={packageStyles.packageCard}>
                 <h3>Premium Tier</h3>
                 <p>This is our premium subscription tier that includes ...</p>
                 <ul>
-                    <li>Unlimited products</li>
-                    <li>access to post product...?</li>
-                    <li>access to premium beta testers</li>
-                    <li>Alert system for testers?</li>
-                    <li>Something else!</li>
+                    <li>Post unlimited amount of products to our beta testers</li>
+                    <li>Alert system for testers</li>
+                    <li>+ Basic Tier features</li>
                 </ul>
-                <button type="button">Select</button>
+                <button type="button" style={packageStyles.submit}>Select</button>
             </div>
         </div>
     );
