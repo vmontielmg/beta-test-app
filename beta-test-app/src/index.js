6c50624297b6c088d7244f5ab4d03c8e76c0dd15
Aveline Estié <aa.estie@gmail.com>
2022-06-12 18:01:31 +0200

    adding firebase init

--- beta-test-app/src/index.js
+++ beta-test-app/src/index.js
@@ -3,6 +3,30 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCkScENHD3ipWfnGrNW8f6wfSzOLVDScmY",
+  authDomain: "beta-test-app.firebaseapp.com",
+  projectId: "beta-test-app",
+  storageBucket: "beta-test-app.appspot.com",
+  messagingSenderId: "519416498562",
+  appId: "1:519416498562:web:5be8b843a4d0d0337a679b",
+  measurementId: "G-BRTZGKT44Z"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
+export const auth = getAuth(app);
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
