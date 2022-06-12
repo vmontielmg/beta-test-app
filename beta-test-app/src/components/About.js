b997bffd3dcd750afc6d02205ce6380eb7b3c715
Vanessa Montiel <32447570+vmontielmg@users.noreply.github.com>
2022-06-12 13:54:08 -0700

    basic about info

--- beta-test-app/src/components/About.js
+++ beta-test-app/src/components/About.js
@@ -1,12 +1,28 @@
 function About() {
+    const styles = {
+        title: {
+            color: "#0D3B66"
+        },
+        about: {
+            border: '1px solid #000',
+            borderRadius: 30,
+            boxShadow: "-1px 5px 5px #0D3B66",
+            display: "inline-block",
+            width: "70%",
+            height: 230,
+            margin: 3,
+            marginTop: "10%",
+            padding: 5,
+            backgroundColor: "#FFFEFB"
+        }
+    }
+
     return (
-        <div>
-            <h1>About us</h1>
+        <div className="about" style={styles.about}>
+            <h1 style={styles.title}>About us</h1>
             <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            AccPower is a website that connects companies with beta testers comprised of marginalised members of society that could potentially be their customer. <br />
+            Our app differs from the rest because our focus is on digital accessibility!
             </p>
         </div>
     );
